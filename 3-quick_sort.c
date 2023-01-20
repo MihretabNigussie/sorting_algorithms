3ce07fec43270aee42c5285b9f8c0beb0f996e79
MihretabNigussie <meresenior14232314@gmail.com>
2023-01-20 15:31:19 -0800

    a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,10 +13,12 @@
 void swap(int *array, size_t size, int *a, int *b)
 {
 	if (*a != *b)
+	{
 		*a = *a + *b;
 		*b = *a - *b;
 		*a = *a - *b;
 		print_array((const int *)array, size);
+	}
 }
 /**
  * lomuto_partition - partitions the array
@@ -34,7 +36,9 @@ size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
 	for (i = j = lo; j < hi; j++)
 	{
 		if (array[j] < pivot)
+		{
 			swap(array, size, &array[j], &array[i++]);
+		}
 	}
 	swap(array, size, &array[i], &array[hi]);
 	return (i);
@@ -68,6 +72,8 @@ void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
 void quick_sort(int *array, size_t size)
 {
 	if (!array || !size)
+	{
 		return;
+	}
 	quicksort(array, size, 0, size - 1);
 }
